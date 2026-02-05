70f79f21509060cdbcf8a48966e0b9f15c298961
alt1236 <apt5919@psu.edu>
2026-02-05 10:57:49 -0500

    Created JavaDocs

--- src/Monsters/Main.java
+++ src/Monsters/Main.java
@@ -8,40 +8,40 @@ public class Main {
         undead.setSkin();
         undead.setAge(104);
         undead.setSpecialPowers(Special_Powers.STRENGTH);
-        undead.setNumberOfEyes();
-        undead.setHealth();
+        undead.setNumberOfEyes(2);
+        undead.setHealth(56);
 
         Monster undead = new Undead();
         undead.setName("Ghost");
         undead.setSkin();
         undead.setAge(26);
         undead.setSpecialPowers(Special_Powers.INVISIBILITY);
-        undead.setNumberOfEyes();
-        undead.setHealth();
+        undead.setNumberOfEyes(0);
+        undead.setHealth(87);
 
         Monster beasts = new Beasts();
         beasts.setName("Dragon");
         beasts.setSkin();
-        beasts.setAge(2004);
+        beasts.setAge(2468);
         beasts.setSpecialPowers(Special_Powers.FIRE_BREATH);
-        undead.setNumberOfEyes();
-        undead.setHealth();
+        undead.setNumberOfEyes(2);
+        undead.setHealth(100);
 
         Monster giants = new Giants();
         giants.setName("Troll");
         giants.setSkin();
-        giants.setAge();
+        giants.setAge(1647);
         giants.setSpecialPowers(Special_Powers.STRENGTH);
-        undead.setNumberOfEyes();
-        undead.setHealth();
+        undead.setNumberOfEyes(3);
+        undead.setHealth(74);
 
         Monster giants = new Giants();
         giants.setName("Cyclops");
         giants.setSkin();
-        giants.setAge();
+        giants.setAge(3637);
         giants.setSpecialPowers(Special_Powers.DURABILITY);
-        undead.setNumberOfEyes();
-        undead.setHealth();
+        undead.setNumberOfEyes(1);
+        undead.setHealth(68);
     }
 
 }
